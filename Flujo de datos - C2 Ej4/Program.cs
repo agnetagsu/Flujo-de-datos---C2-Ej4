3ec515bb5123dd0ff826f64a53c042cc8088f50f
Agneta <agnetagsu@yahoo.es>
2021-05-22 20:04:27 +0200

    Agregar archivos de proyecto.

---
+++ Flujo de datos - C2 Ej4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Flujo_de_datos___C2_Ej4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe un número ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe otro número ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero1 > numero2)
+            {
+                Console.WriteLine("{0} es mayor que {1}", numero1, numero2);
+            }
+            else
+            {
+                if (numero1 < numero2)
+                {
+                    Console.WriteLine("{0} es mayor que {1}", numero2, numero1);
+                }
+                else
+                {
+                    Console.WriteLine("{0} es igual que {1}", numero1, numero2);
+                }
+            }
+        }
+    }
+}
